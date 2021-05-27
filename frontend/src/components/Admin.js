502ff0804a3362da79d859135c42ff3f9e9821ce
davewg1 <78000662+davewg1@users.noreply.github.com>
2021-05-27 09:12:20 +0800

    Added admin settings

--- frontend/src/components/Admin.js
+++ frontend/src/components/Admin.js
@@ -1,11 +1,72 @@
-import React from 'react';
+import React, {Component} from 'react';
+import axios from "axios";
 
-function Admin() {
-    return (
-        <div className='Admin' style={{marginLeft: "14rem"}}>
-            <h1>Admin Settings</h1>
-        </div>
-    );
+export default class Admin extends Component {
+    constructor(p){
+        super(p);
+        this.scrapeNews = this.scrapeNews.bind(this);
+        this.scrapeProduct = this.scrapeProduct.bind(this);
+        this.scrapeBenchmark = this.scrapeBenchmark.bind(this);
+        this.state = {
+            response: ""
+        }
+    }
+
+    scrapeNews()
+    {
+        axios.get('http://localhost:5000/news/scrape')
+            .then(res => {
+                console.log(res.data);
+                this.setState({response: "News Updated"})
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({response: "News Failed to Update"})
+            })
+    }
+
+    scrapeProduct()
+    {
+        axios.get('http://localhost:5000/products/scrape')
+            .then(res => {
+                console.log(res.data);
+                this.setState({response: "Product Updated"})
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({response: "Product Failed to Update"})
+            })
+    }
+
+    scrapeBenchmark()
+    {
+        axios.get('http://localhost:5000/benchmarks/scrape')
+            .then(res => {
+                console.log(res.data);
+                this.setState({response: "Benchmark Updated"})
+            })
+            .catch(err => {
+                console.log(err);
+                this.setState({response: "Benchmark Failed to Update"})
+            })
+    }
+
+    render(){
+        return (
+            <div className='Admin' style={{marginLeft: "16rem",marginTop: "5rem"}}>
+                <h1>ADMIN SETTINGS</h1>
+                <h3 style={{marginTop: "3rem"}}>update contents:</h3>
+                <button className="News" type="button" onclick={this.scrapeNews()}>
+                    News
+                </button>
+                <button className="Product" type="button" onclick={this.scrapeProduct()}>
+                    Product
+                </button>
+                <button className="Benchmarks" type="button" onclick={this.scrapeBenchmark()}>
+                    Benchmarks
+                </button>
+                <h1>{this.state.response}</h1>
+            </div>
+        )
+    }
 }
-
-export default Admin;
